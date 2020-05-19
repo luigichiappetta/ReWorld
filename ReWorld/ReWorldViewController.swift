f3e412df03e4ce9e51616afc90fc114cfce10ee6
Luigi Chiappetta <luluchiappetta9@gmail.com>
2020-05-19 16:40:13 +0200

    PROJECT CREATION

---
+++ ReWorld/ReWorldViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ReWorld
+//
+//  Created by Luigi Chiappetta on 18/05/2020.
+//  Copyright © 2020 Luigi Chiappetta. All rights reserved.
+//
+
+import UIKit
+
+class ReWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didOpenDetail(_ sender: Any) {
+        
+    }
+    
+}
